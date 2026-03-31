e5faaaf0df572fb2465ca830aba85e423648113c
princefrance101-weber <princefrance101@gmail.com>
2026-03-31 16:17:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// 📧 Open Email
+function sendEmail() {
+    window.location.href = "mailto:soundsageaudiobw@gmail.com";
+}
+
+// 📞 Make Phone Call
+function makeCall() {
+    window.location.href = "tel:+26777730365";
+}
+
+// 💬 Open WhatsApp
+function openWhatsApp() {
+    let phone = "26777730365";
+    let message = "Hello, I'm interested in your car audio products!";
+    let url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+    window.open(url, "_blank");
+}
+
+// 📘 Open Facebook
+function openFacebook() {
+    window.open("https://facebook.com/", "_blank"); // 🔁 Replace with your real page link
+}
